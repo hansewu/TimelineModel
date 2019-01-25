269651015f5b5102b70d57cd7ea95a1b66855718
hansewu <nesehoios@163.com>
2019-01-25 16:45:10 +0800

    Basic interfaces

--- TimelineModel.h
+++ TimelineModel.h
@@ -16,8 +16,20 @@ typedef enum
     OverlayTrackType
 } TRACKTYPE;
 
-#define ClipProducer  Mlt::Producer
+typedef enum 
+{
+    ImageClipType = 0,
+    TextClipType,
+    EmptyClipType,
+    AudioOnlyClipType, 
+    VideoOnlyClipType, 
+    VideoAndAudioClipType,
+    ProjectClipType,
+    TransitionClipType 
+} CLIP_TYPE;
 
+#define CLIP_CONTEXT    Mlt::Producer*
+#define FILTER_CONTEXT  Mlt::Filter*
 class TimelineModel 
 {
 public:    
@@ -27,8 +39,8 @@ public:
     int undo();
     int redo();
 
-    int getTrackCount(TRACKTYPE track_type);
-    TRACKTYPE   getTackType(int track_index);
+    int get_track_count(TRACKTYPE track_type);
+    TRACKTYPE   get_tack_type(int track_index);
     int insert_track(int track_index, TRACKTYPE track_type);
     int remove_track(int track_index);
 
@@ -40,17 +52,33 @@ public:
 
     int get_track_duration(int track_index);
     int get_clips_count(int track_index);
-    ClipProducer& get_clip(int track_index, int clip_index);
-    int insert_clip(int track_index, ClipProducer& clip, int position);
+    
+    CLIP_CONTEXT get_clip(int track_index, int clip_index);
+    int get_clip_index( CLIP_CONTEXT clip_context, int &track_index, int &clip_index);
+    CLIP_TYPE get_clip_type(CLIP_CONTEXT clip_context);
+    int get_clip_position(CLIP_CONTEXT clip_context);
+
+    int insert_clip(int track_index, CLIP_CONTEXT clip_context, int position);
     int move_clip(int from_track, int to_track, int clip_index, int position);
 
-    void split_clip(int track_index, int clip_index, int position);
+    int split_clip(int track_index, int clip_index, int position);
 
     int trim_clip_in(int track_index, int clip_index, int delta, bool ripple);
     int trim_clip_out(int track_index, int clip_index, int delta, bool ripple);
 
-    void add_transition_byTrimIn(int track_index, int clip_index, int delta);
-    void add_transition_byTrimOut(int track_index, int clip_index, int delta);
+    int add_Transition(int trackIndex, int clipIndex, int position);
+    int add_transition_byTrimIn(int track_index, int clip_index, int delta);
+    int add_transition_byTrimOut(int track_index, int clip_index, int delta);
+
+    int remove_transition(int track_index, int clip_index);
+
+    int trim_transition_in(int track_index, int clip_index, int delta);
+    int trim_transition_out(int track_index, int clip_index, int delta);
+
+    int get_filters_count(CLIP_CONTEXT clip_context);
+    int insert_filter(int filter_index, CLIP_CONTEXT clip_context, FILTER_CONTEXT filter_context);
+    int move_filter(CLIP_CONTEXT clip_context, int filter_index_from, int filter_index_to);
+    int remove_filter(CLIP_CONTEXT clip_context, int filter_index);
 }
 #endif
 
