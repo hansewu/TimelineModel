88a97a816df459e6ba2cc365010b86323735a072
hansewu <nesehoios@163.com>
2019-03-01 17:14:23 +0800

    add multi_index = -1 in mltext_consumer.h

--- mltext/include/mltext_consumer.h
+++ mltext/include/mltext_consumer.h
@@ -51,12 +51,12 @@ namespace Mlt
 			int set_buffer(int count, int multi_index = -1);   //m_consumer->set("buffer", 25);
 			int set_deinterlace_method(const char* method, int multi_index = -1);//MLT.consumer()->set("deinterlace_method", method);
 			int set_target_name(const char *name, int multi_index = -1); //MLT.consumer()->set("1.target", target.toUtf8().constData());
-			int set_prefill_state(int state); // m_consumer->set("prefill", 1);
+			int set_prefill_state(int state, int multi_index = -1); // m_consumer->set("prefill", 1);
 
-			int set_keyer(int item); //???? keyer  m_consumer->set("keyer", property("keyer").toInt());
-			int set_progressive(int progressive);
+			int set_keyer(int item, int multi_index = -1); //???? keyer  m_consumer->set("keyer", property("keyer").toInt());
+			int set_progressive(int progressive, int multi_index = -1);
 
-			int set_rescale_method(const char *method);
+			int set_rescale_method(const char *method, int multi_index = -1);
 			
 			/*
 			                p->set("rescale", "nearest");
@@ -70,14 +70,14 @@ namespace Mlt
             default:
                 p->set("rescale", "hyper");*/
 
-			int set_strict_method(const char *method);//MLT.consumer()->set("1.strict", "experimental")
-			int set_volume(double volume); //m_consumer->set("volume", volume);
-			int set_image_format(const char *image_format);//m_consumer->set("mlt_image_format", "yuv422");
-			int set_color_trc(const char *color_trc); //m_consumer->set("color_trc", Settings.playerGamma().toLatin1().constData());
-			int set_terminate_on_pause(int enable);//m_consumer->set("terminate_on_pause", 0);
-			int set_real_time(int enable);// m_consumer->set("real_time", realTime());
-			int set_scrub_audio(int enable);//m_consumer->set("scrub_audio", scrubAudio);
-			int set_refresh(int enable);//m_consumer->set("refresh", 1);
+			int set_strict_method(const char *method, int multi_index = -1);//MLT.consumer()->set("1.strict", "experimental")
+			int set_volume(double volume, int multi_index = -1); //m_consumer->set("volume", volume);
+			int set_image_format(const char *image_format, int multi_index = -1);//m_consumer->set("mlt_image_format", "yuv422");
+			int set_color_trc(const char *color_trc, int multi_index = -1); //m_consumer->set("color_trc", Settings.playerGamma().toLatin1().constData());
+			int set_terminate_on_pause(int enable, int multi_index = -1);//m_consumer->set("terminate_on_pause", 0);
+			int set_real_time(int enable, int multi_index = -1);// m_consumer->set("real_time", realTime());
+			int set_scrub_audio(int enable, int multi_index = -1);//m_consumer->set("scrub_audio", scrubAudio);
+			int set_refresh(int enable, int multi_index = -1);//m_consumer->set("refresh", 1);
 
 			
         
